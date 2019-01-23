c4b1792c1d799a4014c990089d93073e7c4a8c26
sihuayin <huayin1018@126.com>
2019-01-23 11:02:19 +0800

    tmx 地图

--- levelSelect.js
+++ levelSelect.js
@@ -2,18 +2,28 @@ var controls
 
 class levelSelect {
   preload () {
+    this.load.tilemapTiledJSON('map', 'res/ChooseLevel/Map/TiledMap.json');
     // res/ChooseLevel/Map/stage_map_" + i + ".png
     for (var i = 0; i < 14; i++) {
       this.load.image(`stage_map_${i}`, "res/ChooseLevel/Map/stage_map_" + i + ".png");
     }
+
+    this.load.image('choose_level_adv','res/ChooseLevel/stagepoint_adv.png')
+    this.load.image('choose_level_boss', 'res/ChooseLevel/stagepoint_gate.png')
+    this.load.image('choose_level_time', 'res/ChooseLevel/stagepoint_time.png')
+    this.load.image('choose_level_chance', 'res/ChooseLevel/stagepoint_chance.png')
   }
   create () {
     // todo 判断 BGMusic 音乐是否播放，如果没有 那么开始播放
     let nextPosX = 0
     var cam = this.cameras.main;
+    var map = this.add.tilemap('map')
+    var tiles = []
 
     this.bgContainer = this.add.container(0, 0)
     for (var i = 0; i < 14; i++) {
+      var tile = map.addTilesetImage(`stage_map_${i}`);
+      tiles.push(tile)
       let imageView = this.add.sprite(0, 0, `stage_map_${i}`).setInteractive({ draggable: true })
       imageView.setOrigin(0, 0.5)
       imageView.setPosition(nextPosX, this.cameras.main.height / 2);
@@ -25,7 +35,7 @@ class levelSelect {
     // this.bgContainer.setSize(nextPosX, this.cameras.main.height)
     this.cameras.main.setBounds(0, 0, nextPosX, this.cameras.main.height);
     var cursors = this.input.keyboard.createCursorKeys();
-
+    var layer = map.getObjectLayer('point');
 
     var controlConfig = {
       camera: this.cameras.main,
@@ -56,6 +66,35 @@ class levelSelect {
     // this.input.on('dragend', function (arg) {
     //   console.log(arg)
     // })
+
+    let objs = layer.objects;
+    for (var i = 0; i < objs.length; i++) {
+        var button = this.add.sprite(0,0, 'choose_level_adv').setInteractive();
+        // this.scorllView.addChild(button, this.zOrderMap.levelButton, i);
+        // this.routeButtonArray.push(button);
+
+        // 编辑器中配置的属性
+        if (objs[i].isBoos == "YES") {
+          button.setTexture('choose_level_boss')
+        }else if (objs[i].isTime == "YES") {
+          button.setTexture('choose_level_time')
+        }else if (objs[i].isChange == "YES"){
+          button.setTexture('choose_level_chance')
+        }
+
+        button.setPosition(objs[i].x, objs[i].y);
+        ((i) => {
+          button.on('pointerdown', (a, b ,c ,d) => { 
+            console.log(i, a, b, c, d)
+            // 停止音乐
+            // 关卡设置
+            // 进入不同等级的游戏
+          }, this)
+        })(i)
+
+        // button.setTag(i);
+        // button.addTouchEventListener(this.onLevelButtonEvent, this);
+    }
   }
 
   update (time, delta) {
