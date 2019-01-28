812137212fd474bd851cc183d921bc3fe78b2a09
sihuayin <huayin1018@126.com>
2019-01-28 12:08:48 +0800

    障碍物

--- GamePlay.js
+++ GamePlay.js
@@ -5,6 +5,8 @@ class gamePlayScene extends Phaser.Scene {
       super();
       this.ZOrderEnum = {};
       this.tiledMapRectMapEnemu = {};
+      this.tiledMapRectArray = [];
+      this.tiledMapRectArrayMap = [];
   }
   preload () {
     this.load.atlas('megaset', 'res/GamePlay/Carrot/Carrot1/hlb1.png', 'res/GamePlay/Carrot/Carrot1/hlb1.json');
@@ -19,10 +21,19 @@ class gamePlayScene extends Phaser.Scene {
     this.load.image('start_bt', 'res/GamePlay/Object/Theme1/Object/start01.png')
     this.load.image('start_carrot_hp_bg', 'res/GamePlay/carrot_hp_bg.png')
     this.load.image('blood_number', 'res/Font/num_blood.png')
+    for(let i = 1; i < 4; i++) {
+      this.load.image(`S${i}`, `res/GamePlay/Object/Theme1/Object/S${i}.png`)
+    }
+    for(let i = 1; i < 4; i++) {
+      this.load.image(`L${i}`, `res/GamePlay/Object/Theme1/Object/L${i}.png`)
+    }
+    for(let i = 1; i < 3; i++) {
+      this.load.image(`B${i}`, `res/GamePlay/Object/Theme1/Object/B${i}.png`)
+    }
     this.load.tilemapTiledJSON({
       key: 'map',
       url: 'res/GamePlay/Theme/Theme1/BG1/Level1.json'
-  });
+    });
   }
 
   create () {
@@ -54,6 +65,10 @@ console.log(frames)
     this.loadEndButton()
     this.loadBloodBg()
     this.loadBlood()
+    this.loadCanTouchRect()
+    this.loadTiledMapRectArrayMap()
+    this.loadRoadPointArray()
+    this.loadObstacle()
   }
 
   loadProperty () {
@@ -149,6 +164,163 @@ console.log(frames)
 
     this.cache.bitmapFont.add('blood_number', Phaser.GameObjects.RetroFont.Parse(this, config));
     this.add.bitmapText(this.carrot.x + 42, this.carrot.y + 40, 'blood_number', '10');
+  }
+
+  loadCanTouchRect () {
+    var mapWidth = this.map.width;
+    var mapHeight = this.map.height;
+    var winSize = this.cameras.main;
+    var nextPosX = (winSize.width - this.map.widthInPixels) / 2 + this.map.tileWidth / 2;
+    var nextPosY = (winSize.height - this.map.heightInPixels) / 2 + this.map.tileHeight / 2;
+
+    for (var i = 0; i < mapHeight; i++) {
+      this.tiledMapRectArray[i] = [];
+      for (var j = 0; j < mapWidth; j++) {
+          // 空地
+          this.tiledMapRectArray[i][j] = new Phaser.Geom.Rectangle(nextPosX - this.map.tileWidth / 2, nextPosY - this.map.tileHeight / 2, this.map.tileWidth, this.map.tileHeight);
+
+          //node = new cc.Sprite();
+          //this.addChild(node, 200);
+          //node.setTextureRect(cc.rect(0, 0, tileSize.width - 2, tileSize.height - 2));
+          //node.setColor(cc.color(122, 122, 255));
+          //node.setPosition(nextPosX, nextPosY);
+          //node.setOpacity(120);
+
+          nextPosX += this.map.tileWidth;
+      }
+
+      nextPosX = (winSize.width - this.map.widthInPixels) / 2 + this.map.tileHeight / 2;
+      nextPosY += this.map.tileHeight;
+    }
+    console.log(this.tiledMapRectArray)
+  }
+
+  loadTiledMapRectArrayMap () {
+    var i;
+    let tileHeight = this.map.tileHeight
+    let tileWidth = this.map.tileWidth
+    for (i = 0; i < tileHeight; i++) {
+        this.tiledMapRectArrayMap[i] = [];
+        for (var j = 0; j < tileWidth; j++) {
+            this.tiledMapRectArrayMap[i][j] = this.tiledMapRectMapEnemu.NONE;
+        }
+    }
+  }
+
+  //  offset 被去掉，不知道是否会影响显示
+  loadRoadPointArray () {
+    this.roadPointArray = [];
+    var roadGroup = this.map.getObjectLayer("road");
+    var roads = roadGroup.objects;
+
+    for (var i = 0; i < roads.length; i++) {
+        this.roadPointArray.push(new Phaser.Geom.Rectangle(roads[i].x, roads[i].y));
+    }
+  }
+
+  loadObstacle () {
+    this.loadSmallObstacle()
+    this.loadLittleObstacle()
+    this.loadBigObstacle()
+    this.loadInvalidRect()
+  }
+
+  loadSmallObstacle () {
+    var smallGroup = this.map.getObjectLayer("small");
+    var smalls = smallGroup.objects;
+    var node = null;
+    var info = null;
+    for (var i = 0; i < smalls.length; i++) {
+
+        node = this.add.sprite(smalls[i].x + 147, smalls[i].y + 50, smalls[i].name)
+
+
+        info = this.getInfoFromMapByPos(node.x, node.y);
+        this.tiledMapRectArrayMap[info.row][info.cel] = this.tiledMapRectMapEnemu.SMALL;
+    }
+  }
+
+  loadLittleObstacle (){
+    var littleGroup = this.map.getObjectLayer("little");
+    var objs = littleGroup.objects;
+    var node = null;
+    var info = null;
+    for (var i = 0; i < objs.length; i++) {
+      node = this.add.sprite(objs[i].x + 110, objs[i].y + 40, objs[i].name)
+
+
+      info = this.getInfoFromMapByPos(node.x, node.y);
+      this.tiledMapRectArrayMap[info.row][info.cel] = this.tiledMapRectMapEnemu.LITTLE;
+      this.tiledMapRectArrayMap[info.row][info.cel - 1] = this.tiledMapRectMapEnemu.LITTLE;
+    }
+  }
+
+  loadBigObstacle () {
+    var bigGroup = this.map.getObjectLayer("big");
+    var objs = bigGroup.objects;
+    var node = null;
+    var info = null;
+    for (var i = 0; i < objs.length; i++) {
+      node = this.add.sprite(objs[i].x + 100, objs[i].y + 80, objs[i].name)
+
+        info = this.getInfoFromMapByPos(node.x, node.y);
+        this.tiledMapRectArrayMap[info.row][info.cel] = this.tiledMapRectMapEnemu.BIG;
+        this.tiledMapRectArrayMap[info.row][info.cel - 1] = this.tiledMapRectMapEnemu.BIG;
+        this.tiledMapRectArrayMap[info.row - 1][info.cel] = this.tiledMapRectMapEnemu.BIG;
+        this.tiledMapRectArrayMap[info.row - 1][info.cel - 1] = this.tiledMapRectMapEnemu.BIG;
+    }
+  }
+
+  loadInvalidRect () {
+    var invalidGroup = this.map.getObjectLayer("invalid");
+    var invalids = invalidGroup.objects;
+    var data = null;
+    var info = null;
+    for (var i = 0; i < invalids.length; i++) {
+        data = invalids[i];
+        // data.x += invalidGroup.getPositionOffset().x;
+        // data.y += invalidGroup.getPositionOffset().y;
+
+        info = this.getInfoFromMapByPos(data.x, data.y);
+        if (info.isInMap) {
+          this.tiledMapRectArrayMap[info.row][info.cel] = this.tiledMapRectMapEnemu.INVALID;
+        }
+        
+    }
+  }
+  // 根据坐标获取在地图中的信息
+  getInfoFromMapByPos (x, y){
+    if (y === undefined) {
+      console.log("GPMainLayer.getInfoFromMapByPos(): Y坐标不能为空！")
+    }
+
+    var isInMap = false;
+    var index = {
+        x : -1,
+        y : -1
+    };
+    var rect = null;
+    for (var i = 0; i < this.tiledMapRectArray.length; i++) {
+        for (var j = 0; j < this.tiledMapRectArray[i].length; j++) {
+            rect = this.tiledMapRectArray[i][j];
+            if (Phaser.Geom.Rectangle.ContainsPoint(rect, new Phaser.Geom.Point(x, y))) {
+                index.row = i;
+                index.cel = j;
+                index.x = rect.x;
+                index.y = rect.y;
+                isInMap = true;
+                console.log("GPMainLayer.getInfoFromMapByPos() : rect is ", rect);
+            }
+        }
+    }
+
+    return {
+        isInMap : isInMap,
+        row : index.row,  // 行
+        cel : index.cel,  // 列
+        x : index.x,
+        y : index.y
+    };
   }
   update () {}
 }
